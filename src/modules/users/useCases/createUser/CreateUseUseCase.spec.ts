5bc9959d99767ee14532b48469e69490dc164023
Raquel Moreira <raquel.moreira@nextpage.com.br>
2022-03-25 18:27:05 -0300

    teste unitário crear novo usuario

---
+++ src/modules/users/useCases/createUser/CreateUseUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: InMemoryUsersRepository;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "test",
+      email: "test",
+      password: "test",
+    });
+    console.log(user);
+    expect(user).toHaveProperty("id");
+  });
+});
